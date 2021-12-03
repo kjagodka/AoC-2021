0b06b936ba45e7f573e4f006363dd75d388eaeec
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2021-12-03 08:38:38 +0100

    cosmetic changes

--- src/Day03.hs
+++ src/Day03.hs
@@ -4,30 +4,30 @@ import Data.Char (digitToInt)
 count :: Eq a => a -> [a] -> Int
 count elem list = length $ elemIndices elem list
 
-mostCommonBits :: [[Char]] -> [Char]
-mostCommonBits lns = map (\col -> if count '1' col >= count '0' col then '1' else '0' ) $ transpose lns
+mostCommonBits :: [[Int]] -> [Int]
+mostCommonBits lns = map (\col -> if count 1 col >= count 0 col then 1 else 0 ) $ transpose lns
 
-leastCommonBits :: [[Char]] -> [Char]
-leastCommonBits lns = map (\digit -> if digit == '1' then '0' else '1' ) $ mostCommonBits lns
+leastCommonBits :: [[Int]] -> [Int]
+leastCommonBits lns = map (1 - ) $ mostCommonBits lns
 
-binaryToInt :: [Char] -> Int
-binaryToInt = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+binaryToInt :: [Int] -> Int
+binaryToInt = foldl' (\acc x -> acc * 2 + x) 0
 
-findRating :: ([[Char]] -> [Char]) -> [[Char]] -> [Char]
-findRating bitSelector lns = findRating' 0 lns
+findRating :: ([[Int]] -> [Int]) -> [[Int]] -> [Int]
+findRating bitSelector numbers = findRating' 0 numbers
   where
-    len = length . head $ lns
+    len = length . head $ numbers
     findRating' _ [x] = x
-    findRating' index lns =
-      let filterBit = bitSelector lns !! index
-      in findRating' ((index + 1) `mod` len) $ filter (\line -> line !! index == filterBit) lns
+    findRating' index numbers =
+      let filterBit = bitSelector numbers !! index
+      in findRating' ((index + 1) `mod` len) $ filter (\line -> line !! index == filterBit) numbers
 
 
 main = do {
   dta <- getContents;
-  let numbers = lines dta
+  let numbers = map (map digitToInt) $ lines dta
       gammaRate = (binaryToInt . mostCommonBits $ numbers) * (binaryToInt . leastCommonBits $ numbers)
       oxygenRating = binaryToInt $ findRating mostCommonBits numbers
       co2Rating = binaryToInt $ findRating leastCommonBits numbers
-  in print (gammaRate, oxygenRating * co2Rating)
+  in print (gammaRate, oxygenRating * co2Rating);
   }
