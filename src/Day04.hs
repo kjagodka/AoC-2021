4c5686c87ef642561442e157e17d66273eaf72a7
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2021-12-05 01:34:57 +0100

    Solved Problem 04A

---
+++ src/Day04.hs
@@ -0,0 +1,41 @@
+import Data.List.Split
+import Data.List ( inits, minimumBy, transpose )
+import Data.Ord (comparing)
+import qualified Data.Bifunctor
+
+countCommonElements :: Eq a => [a] -> [a] -> Int
+countCommonElements xs ys =
+  length $ filter (`elem` ys) xs
+
+isBoardWinning :: Eq a => [[a]] -> [a] -> Bool
+isBoardWinning board numbers =
+  any (\row -> boardSize == countCommonElements numbers row) board ||
+  any (\col -> boardSize == countCommonElements numbers col) (transpose board)
+  where boardSize = length . head $ board
+
+
+
+numbersToWin :: Eq a => [[a]] -> [a] -> Int
+numbersToWin board numbers =
+  let winningLists = filter (isBoardWinning board) $ inits numbers
+  in
+    length . head $ winningLists
+
+findWinningBoard :: Eq a => [[[a]]] -> [a] -> [[a]]
+findWinningBoard boards numbers =
+  minimumBy (comparing (`numbersToWin` numbers)) boards
+
+remainingNumbers :: (Eq a) => [[a]] -> [a] -> [[a]]
+remainingNumbers board numbers = 
+  map (filter (`notElem` numbers)) board
+
+
+main = do
+  inp <- getContents
+  let numbers = map read $ splitOn "," $ head $ lines inp :: [Int]
+      boards = splitOn [[]] $ map (map read . words) . tail . tail $ lines inp :: [[[Int]]]
+      winningBoard = findWinningBoard boards numbers
+      winningBoardNumbers = take (numbersToWin winningBoard numbers) numbers
+      answer1 = last winningBoardNumbers * (sum . concat $ remainingNumbers winningBoard winningBoardNumbers)
+
+  print answer1
