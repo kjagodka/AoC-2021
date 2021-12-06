ed1d18ec413304e26484fbd5ea3dd8a338318267
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2021-12-06 21:36:11 +0100

    better naming

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,28 +1,28 @@
-import Data.List (elemIndices)
+import Data.List(elemIndices)
 import qualified Data.Sequence as S
 import Data.List.Split (splitOn)
 
-newtype LanternfishPop = AgesPops (S.Seq Int)
+newtype FishPopulation = FishPopulation (S.Seq Int)
 
 count :: Eq a => [a] -> a -> Int
 count list elem = length $ elemIndices elem list
 
-lanternfishPopFromList :: [Int] -> LanternfishPop
-lanternfishPopFromList fishList = AgesPops (S.fromFunction 9 $ count fishList)
+fishListToPopulation :: [Int] -> FishPopulation
+fishListToPopulation fishList = FishPopulation (S.fromFunction 9 $ count fishList)
 
-countFish :: LanternfishPop -> Int
-countFish (AgesPops counts) = sum counts
+countFish :: FishPopulation -> Int
+countFish (FishPopulation counts) = sum counts
 
-updatePopulation :: LanternfishPop -> LanternfishPop
-updatePopulation (AgesPops counts) =
-  let fertile = S.index counts 0
+updatePopulation :: FishPopulation -> FishPopulation
+updatePopulation (FishPopulation counts) =
+  let reproducers = S.index counts 0
       others = S.drop 1 counts
-   in AgesPops (S.insertAt 8 fertile . S.adjust' (+ fertile) 6 $ others)
+   in FishPopulation (S.adjust' (+ reproducers) 6 $ S.insertAt 8 reproducers others)
 
 main :: IO ()
 main = do
   inp <- getContents
-  let lanternfishInitPopulation = lanternfishPopFromList . map read . splitOn "," $ inp
-      populationAfterNDays = iterate updatePopulation lanternfishInitPopulation
+  let initPopulation = fishListToPopulation . map read $ splitOn "," inp
+      populationAfterNDays = iterate updatePopulation initPopulation
   print . countFish $ populationAfterNDays !! 80
   print . countFish $ populationAfterNDays !! 256
