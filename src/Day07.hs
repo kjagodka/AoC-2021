d1ee8fe40d9123d19034991655cb8982ec1d0ffb
Cyryl Waśkiewicz <cyryl.was@gmail.com>
2021-12-07 10:14:29 +0100

    implemented binary search of global minimum

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 import Data.Functor (fmap)
 import Data.List (sort)
+import Language.LSP.Types.Lens (HasPositions (positions))
 
-newtype Fuel = Fuel Int deriving (Eq, Ord, Num, Show)
+newtype Fuel = Fuel Int deriving (Eq, Ord, Show, Num)
+
 newtype Position = Position Int deriving (Eq, Ord, Enum)
+
+newtype Range = Range (Position, Position)
+
 newtype Distance = Distance Int
 
 distance :: Position -> Position -> Distance
@@ -21,16 +27,44 @@ standardMetric from to = standardFuelUsage $ distance from to
 crabMetric :: Position -> Position -> Fuel
 crabMetric from to = crabFuelUsage $ distance from to
 
+getRange :: [Position] -> Range
+getRange positions = Range (minimum positions, maximum positions)
+
+isSingleton :: Range -> Bool
+isSingleton (Range (Position a, Position b)) = a == b
+
+midPoint :: Range -> Position
+midPoint (Range (Position a, Position b)) = Position ((a + b) `div` 2)
+
+leftOf :: Range -> Position -> Range
+leftOf (Range (from, _)) pos = Range (from, pos)
+
+rightOf :: Range -> Position -> Range
+rightOf (Range (_, to)) (Position pos) = Range (Position (pos + 1), to)
+
 checkFuelUsage :: (Position -> Position -> Fuel) -> [Position] -> Position -> Fuel
 checkFuelUsage metric startPositions meetingPos =
   sum $ map (metric meetingPos) startPositions
 
-minimalFuelUsage :: (Position -> Position -> Fuel) -> [Position] -> Fuel
-minimalFuelUsage metric startingPositions =
-  minimum $ map (checkFuelUsage metric startingPositions) [min .. max]
+optimalPosition :: (Position -> Position -> Fuel) -> [Position] -> Range -> Position
+optimalPosition metric startingPositions range
+  | isSingleton range = mid
+  | checkFuelUsage metric startingPositions mid
+      > checkFuelUsage
+        metric
+        startingPositions
+        ( succ mid
+        ) =
+    optimalPosition metric startingPositions $ rightOf range mid
+  | otherwise = optimalPosition metric startingPositions $ leftOf range mid
   where
-    min = minimum startingPositions
-    max = maximum startingPositions
+    mid = midPoint range
+
+minimalFuelUsage :: (Position -> Position -> Fuel) -> [Position] -> Fuel
+minimalFuelUsage metric positions =
+  let range = getRange positions
+      optimalPos = optimalPosition metric positions range
+   in checkFuelUsage metric positions optimalPos
 
 main :: IO ()
 main = do
